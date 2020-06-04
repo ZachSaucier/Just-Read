1bd6ffeb74a44736ce97fb16b50f29e44f9d923d
ZachSaucier <zachsaucier@gmail.com>
2020-06-04 15:45:30 -0400

    Improved default styles

--- content_script.js
+++ content_script.js
@@ -1524,7 +1524,7 @@ function continueLoading() {
 /////////////////////////////////////
 var isPaused = false,
     stylesheetObj = {},
-    stylesheetVersion = 1.31; // THIS NUMBER MUST BE CHANGED FOR THE STYLESHEETS TO KNOW TO UPDATE
+    stylesheetVersion = 1.32; // THIS NUMBER MUST BE CHANGED FOR THE STYLESHEETS TO KNOW TO UPDATE
 
 function launch() {
     // Detect past overlay - don't show another
